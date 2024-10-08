9b4224d715f8dbd164116463f14ce9eb4fc4d117
Atuhaire Collins Benda <collinsbenda360@gmail.com>
2024-10-08 11:21:24 +0300

    feat: add og image url builder

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,5 +1,6 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import type { Image } from 'sanity'
 
 import { dataset, projectId } from '../env'
 
@@ -8,4 +9,17 @@ const builder = createImageUrlBuilder({ projectId, dataset })
 
 export const urlFor = (source: SanityImageSource) => {
   return builder.image(source)
+}
+
+export const urlForImage = (source: Image | undefined) => {
+  // Ensure that source image contains a valid reference
+  if (!source?.asset?._ref) {
+    return undefined
+  }
+
+  return builder?.image(source).auto('format').fit('max')
+}
+
+export function urlForOpenGraphImage(image: Image | undefined) {
+  return urlForImage(image)?.width(1200).height(627).fit('crop').url()
 }
